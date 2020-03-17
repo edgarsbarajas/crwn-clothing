d84208057373548e2d8dcc325a9becc666d26909
Edgar Barajas <edgar_barajas@gap.com>
2020-03-16 21:08:31 -0700

    Update app with shop page, preview component, and collection item component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom';
+
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
